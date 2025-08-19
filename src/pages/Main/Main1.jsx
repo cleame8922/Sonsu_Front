a5709e761b96f53c9eb3699cda190fbc27aaa6c5
이호연 <ihoyeon@ihoyeon-ui-MacBookAir.local>
2025-08-19 19:39:51 +0900

    [fix]Main

---
+++ src/pages/Main/Main1.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+
+export default function Main1() {
+  return (
+    <div className="flex flex-col mt-40 justify-center">
+      {/* 상단 텍스트 */}
+      <div className="items-center flex flex-col ">
+        <div className="text-[#3c3c3c] text-[25px]">
+          손으로 이어지는 새로운 소통
+        </div>
+        <div className="text-[#121212] text-[80px] font-bold mb-20">
+          손手잇다
+        </div>
+        <div className="flex space-x-6 mt-2">
+          <img src="/images/Main/AppStore.png" className="w-[170px] h-auto" />
+          <img src="/images/Main/GooglePlay.png" className="w-[170px] h-auto" />
+        </div>
+      </div>
+
+      {/* 목업 이미지 (고정 배치) */}
+      <div className="flex justify-center items-end mt-20 space-x-[30px]">
+        {/* <img
+          src="/images/Mockup/Bookmark.png"
+          className="w-[300px] h-auto rotate-[-20deg] drop-shadow-2xl translate-y-[240px]"
+        /> */}
+        <img
+          src="/images/Mockup/Attendance.png"
+          className="w-[300px] h-auto rotate-[-15deg] drop-shadow-2xl translate-y-[100px]"
+        />
+        <img
+          src="/images/Mockup/Main.png"
+          className="w-[300px] h-auto rotate-[-7deg] drop-shadow-2xl translate-y-[30px]"
+        />
+        <img
+          src="/images/Mockup/Study.png"
+          className="w-[300px] h-auto rotate-[0deg] drop-shadow-2xl"
+        />
+        <img
+          src="/images/Mockup/MyPage.png"
+          className="w-[300px] h-auto rotate-[7deg] drop-shadow-2xl translate-y-[30px]"
+        />
+        <img
+          src="/images/Mockup/SpeedGame.png"
+          className="w-[300px] h-auto rotate-[15deg] drop-shadow-2xl translate-y-[100px]"
+        />
+        {/* <img
+          src="/images/Mockup/OX.png"
+          className="w-[300px] h-auto rotate-[20deg] drop-shadow-2xl translate-y-[240px]"
+        /> */}
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/Main/Main2.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function Main2() {
+  return (
+    <div className="h-screen">
+      {/* 텍스트 */}
+      <div className="text-[#404040] text-[25px] leading-10 w-full bg-[#FFE694] h-[400px] flex flex-col items-center justify-center">
+        <p>이제껏 경험하지 못한 쉽고 즐거운 수어 교육 서비스,</p>
+        <p>
+          <span className="text-[#000] font-black text-[30px] mr-1">
+            손수잇다
+          </span>
+          와 함께라면 당신의 배움이 달라질거예요.
+        </p>
+      </div>
+
+      {/* 말풍선 */}
+      <div>
+        <div>#게임처럼_배우는_수어학습</div>
+      </div>
+    </div>
+  );
+}

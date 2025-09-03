be07dd6ca1e73f90941f9c18243e48d830fc2fd1
cleame8922 <cleame8922@sungkyul.ac.kr>
2025-09-03 20:56:38 +0900

    [fix]adminstudent, main수정

---
+++ src/data/students.js
@@ -0,0 +1,34 @@
+const students = [
+  {
+    id: 1,
+    name: '노태경',
+    photo: '/assets/images/peoples/노태경.jpg',
+  },
+  {
+    id: 2,
+    name: '최유정',
+    photo: '/assets/images/peoples/최유정.jpg',
+  },
+  {
+    id: 3,
+    name: '이호연',
+    photo: '/assets/images/peoples/이호연.jpg',
+  },
+  {
+    id: 4,
+    name: '장원석',
+    photo: '/assets/images/peoples/장원석.jpg',
+  },
+  {
+    id: 5,
+    name: '공준석',
+    photo: '/assets/images/peoples/공준석.jpg',
+  },
+  {
+    id: 6,
+    name: '김정이',
+    photo: '/assets/images/peoples/김정이.jpg',
+  },
+];
+
+export default students;

59112faa17e9a2e0e024d0fc11dddcf9773639ad
이호연 <ihoyeon@ihoyeon-ui-MacBookAir.local>
2025-09-05 17:48:20 +0900

    [fix] Curri_Part

--- src/pages/Admin/Main/AdminMain3.jsx
+++ src/pages/Admin/Main/AdminMain3.jsx
@@ -2,16 +2,16 @@ import React from "react";
 
 export default function AdminMain3() {
   return (
-    <div className="h-[1000px] bg-white relative">
+    <div className="h-[800px] bg-white relative">
       <img
         src="/assets/images/Admin/Main/AdminMain3_1.png"
         alt="group"
-        className="absolute w-[800px] top-[300px] left-[220px]"
+        className="absolute w-[700px] top-[250px] left-[220px]"
       />
       <img
         src="/assets/images/Admin/Main/AdminMain3_2.png"
         alt="group"
-        className="absolute w-[1000px] top-[30px] right-[180px]"
+        className="absolute w-[800px] top-[10px] right-[180px]"
       />
     </div>
   );

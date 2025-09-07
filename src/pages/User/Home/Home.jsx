4a9a859a697cb871d4105df6964a4595b9f0188e
이호연 <ihoyeon@ihoyeon-ui-MacBookAir.local>
2025-09-08 00:02:34 +0900

    [fix] Home.jsx api 왜 안대냐

--- src/pages/User/Home/Home.jsx
+++ src/pages/User/Home/Home.jsx
@@ -1,10 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import UserNav from "../../../components/UserNav";
 import UserTitle from "../../../components/UserTitle";
 import DailyCheckIn from "./DailyCheckIn";
 import ContinueLearning from "./ContinueLearning";
+import axios from "axios";
+import { getToken } from "../../../utils/authStorage";
+import { useNavigate } from "react-router-dom";
+import { API_URL } from "../../../config";
 
 export default function Home() {
+  const [userInfo, setUserInfo] = useState({});
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchUserInfo = async () => {
+      try {
+        const token = getToken();
+
+        if (!token) {
+          console.log("토큰이 없습니다 .");
+          return;
+        }
+
+        const response = await axios.get(`${API_URL}/login/success`, {
+          headers: {
+            Authorization: token,
+            "Content-Type": "application/json",
+          },
+          withCredentials: true,
+        });
+
+        if (response.data) {
+          setUserInfo(response.data);
+        }
+      } catch (error) {
+        console.error("사용자 정보 가져오기 실패:", error);
+        console.error("에러 상세:", error.response?.data);
+
+        // 토큰이 만료되었거나 유효하지 않은 경우
+        if (error.response?.status === 401) {
+          console.log("토큰이 만료되었거나 유효하지 않습니다.");
+        }
+      }
+    };
+
+    fetchUserInfo();
+  }, []);
   return (
     <div className="min-h-screen bg-[#D2D2D2]">
       <UserTitle />
@@ -21,7 +62,11 @@ export default function Home() {
                 alt=""
                 className="w-[220px]"
               />
-              <p className="text-[32px] fontMedium mt-12">안녕하세요, OOO님!</p>
+              <p className="text-[32px] fontMedium mt-12">
+                안녕하세요,{" "}
+                {userInfo?.username ? userInfo?.username : "undefined"}
+                님!
+              </p>
             </div>
             <div className="w-[1px] h-[500px] bg-[#D9D9D9] "></div>
             {/* 오 */}
